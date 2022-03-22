a09040e86263d162363d79df84a47de7dfa0a84b
damkols <damolausmankolapo@gmail.com>
2022-03-22 15:51:41 -0800

    built clearCountry button

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid md:grid-cols-2 gap-4">
+  <div class="grid md:grid-cols-2 gap-6">
     <!-- Confirmed cases -->
     <div class="shadow-md bg-blue-100 p-10 text-center rounded">
       <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,14 @@
     <DataBoxes :stats="stats" />
 
     <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="bg-green-700 text-white rounded p-3 mt-6 focus:outline-none hover:bg-green"
+    >
+      Clear Country
+    </button>
   </main>
 
   <main class="flex flex-col align-center justify-center text-center" v-else>
@@ -45,6 +53,14 @@ export default {
     getCountryData(country) {
       this.stats = country;
       this.title = country.Country;
+    },
+
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
